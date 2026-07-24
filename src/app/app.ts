ca89ff4ccccafb8995770769cd9e32ac8d3c8ac6
DOROL - - BEAUROY - EUSTACHE Alexice <ice.flu0rescent333@gmail.com>
2026-07-24 17:00:05 +0200

    Commit origin

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,37 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { Employee } from './employee';
+import { EmployeeService } from './employee.service';
+import { HttpErrorResponse } from '@angular/common/http';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
   templateUrl: './app.html',
+  imports: [
+    CommonModule
+  ],
   styleUrl: './app.css'
 })
-export class App {
-  protected readonly title = signal('employeemanagerapp');
-}
+export class App implements OnInit {
+  //protected readonly title = signal('employeemanagerapp');
+  public employees: Employee[] = [];
+
+  constructor(private employerService: EmployeeService){}
+
+  ngOnInit(): void {
+    this.getEmployees();
+  }
+  
+  // TODO changer le subscribe
+  public getEmployees(): void {
+    this.employerService.getEmployees().subscribe(
+     (response: Employee[]) => {
+        this.employees = response;
+      },
+      (error: HttpErrorResponse) => {
+        alert(error.message);
+      }
+    );
+  }
+ }
+
