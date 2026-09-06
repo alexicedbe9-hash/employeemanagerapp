c798fa7fd874a93eb077918223b73f72be48c7fc
DOROL - - BEAUROY - EUSTACHE Alexice <ice.flu0rescent333@gmail.com>
2026-09-06 14:54:55 +0200

    App.ts : - Ajout d'une nouvelle méthode pour afficher les modals en fonction du mode (add, edit, delete) App.html : - Ajout des scripts Bootstrap App.html : - Ajout des modales (ajouter un nouvel employé, mettre à jour et supprimer). Pour le moment, les boutons de validation sur les modales ne sont pas connectés au back

--- src/app/app.ts
+++ src/app/app.ts
@@ -3,7 +3,6 @@ import { Employee } from './employee';
 import { EmployeeService } from './employee.service';
 import { HttpErrorResponse } from '@angular/common/http';
 import { CommonModule } from '@angular/common';
-import { response } from 'express';
 
 @Component({
   selector: 'app-root',
@@ -33,5 +32,28 @@ export class App implements OnInit {
       }
     })
   }
+
+  public onOpenModal(employee : Employee | null, mode: string): void {
+    const container = document.getElementById("main-container");  
+    const button = document.createElement('button');
+
+    button.type = "button";
+    button.style.display = "none"
+    button.setAttribute("data-toggle", "modal");
+    if (mode == 'add'){
+      button.setAttribute('data-target', '#addEmployeeModal');
+    }
+    if (mode == 'edit'){
+      button.setAttribute('data-target', '#editEmployeeModal');
+    }
+    if (mode == 'delete'){
+      button.setAttribute('data-target', '#deleteEmployeeModal');
+    }
+
+    container?.appendChild(button);
+    button.click();
+  }
  }
+
+
 
